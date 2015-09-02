0f5809a82a73943eb0d2f5067f51dc74bfe2e601
leochen4891 <leochen4891@gmail.com>
2015-09-01 23:04:33 -0700

    08/19/2015

--- Medium/LinkedListCycle.py
+++ Medium/LinkedListCycle.py
@@ -12,10 +12,19 @@ from ListNode import ListNode
 #         self.val = x
 #         self.next = None
 
+# the idea is to go along the link and reverse the link after passing
+# if there is a loop, it will reach the head eventually
 class Solution(object):
     def hasCycle(self, head):
         """
         :type head: ListNode
         :rtype: bool
         """
+        # size 0 and 1
+        if not head or head.next == None: return False
+        if head.next == head: return True
+
+        # size 2
+        second = head.next
+        if second.next == head or second.next ==
 
